ff37b65cf9628e5a0a4c5e7d8b1bb0855a81e1cb
Matheus Ícaro <matheusicaro2@hotmail.com>
2021-11-06 13:27:25 -0300

    Starting repository with initial settings using TypeScript and Express.

---
+++ back-end/src/app.ts
@@ -0,0 +1,32 @@
+require('dotenv').config();
+import cors = require('cors');
+import morgan = require('morgan');
+import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
+
+import appRouters from './routes/app.routes';
+import environment from './config/environment';
+
+const corsOptions: cors.CorsOptions = {
+  allowedHeaders: ['Authorization', 'Content-Type'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+};
+
+swaggerDocs.host = environment.NODE_ENV === 'production' ? `${environment.HOST_NAME}` : `localhost:${environment.PORT}`;
+
+const app = express();
+
+app.set('env', environment.NODE_ENV);
+app.set('port', environment.PORT);
+app.set('host', environment.HOST_NAME);
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(morgan('common'));
+
+app.get('/', (req, res) => res.redirect('/api-docs'));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use(environment.BASE_PATH, appRouters);
+
+export default app;
